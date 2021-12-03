45e14729d6193a6aa5b88ee1100f2255810742f4
Dalibennour <95385865+Dalibennour@users.noreply.github.com>
2021-12-03 23:56:04 +0100

    Add files via upload

---
+++ projet.py
@@ -0,0 +1,35 @@
+import cv2
+#from gpiozero import buzzer
+#bz = Buzzer(0)
+
+# intitation de la capture por detecter 
+capteur = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+detector = cv2.CascadeClassifier("contour_visage.xml")
+
+while (True):
+ # is_reading:bool etat de le captuer active true,pas active false 
+ # image_capturee:image  l image captuer sous forme d un matrice de pixel
+  is_reading,image_capturee=capteur.read()
+  
+  faces = detector.detectMultiScale(image_capturee)
+    
+ # executer une interface pour charge les image capturee
+  for (x, y, w, h) in faces:
+    cv2.rectangle(image_capturee, (x, y), (x+w, y+h), (0, 255, 0), 2)
+  print('\a')
+  #bz.on()
+    
+  cv2.imshow('projet iot', image_capturee)                                     #afficher les image_capturee sur l'ecran.
+  if cv2.waitKey(1) & ord('d'):
+	  break
+
+
+capteur.release()                         #décharger les images charger de la memoire RAM.
+cv2.destroyAllWindows()                   #fermee touts les fenetres. 
+
+   
+
+
+
+                                       
+                                       
